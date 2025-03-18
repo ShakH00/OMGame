9b4e33229261b878559793450e989153f88ad907
30210711 <ayan.siddiqui1@ucalgary.ca>
2025-03-17 18:17:18 -0600

    Added fields and methods to Game interface

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,17 @@
-public class Game {
+interface Game {
+    Player player1 = new Player();
+    Player player2 = new Player();
+    int score1 = 0, score2 = 0;
+    Board board = new Board();
+    Piece selectedPiece = new Piece();
+    GameState gameState = new GameState();
+    GameRules gameRules = new GameRules();
 
+    public void move(Piece piece);
+
+    public void checkWinCondition();
+
+    public void surrender();
+
+    public void matchOutcome();
 }
