bff3667c59cf0bd0b91dfd675f3a7484ba09d870
30175269 <adam.chan1@ucalgary.ca>
2025-03-26 12:45:53 -0600

    Creating Checkers class and setting up placeholder methods

---
+++ src/Checkers.java
@@ -0,0 +1,28 @@
+public class Checkers{
+    
+    
+    
+    public void move(Piece piece){
+
+    }
+
+    public bool isValidMove(Piece piece){
+
+    }
+
+    public void jumpMove(){
+
+    }
+
+    public void checkWinCondition(){
+
+    }
+
+    public void surrender(){
+
+    }
+
+    public void matchOutcome(){
+
+    }
+}
