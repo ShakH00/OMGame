1954839b9de5307ef985927718aa91cf99ec9140
30203751 <nebila.wako@ucalgary.ca>
2025-03-26 13:25:23 -0600

    Added methods needed for matchmaking and elo calculation

---
+++ src/matchmaking/matchmaking.java
@@ -0,0 +1,17 @@
+public class matchmaking {
+
+    public void updateElo(Player player1, Player player2, int result) {
+    }
+
+    public double calculateExpectedScore(double ratingA, double ratingB) {
+
+    }
+
+    public boolean isMatchSuitable(Player player1, Player player2) {
+        return false;
+    }
+
+    public Player findBestMatch(Player targetPlayer, List<Player> playerPool) {
+        return null;
+    }
+}
