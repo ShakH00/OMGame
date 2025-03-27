0c0a10e85be513649963a45bb23db23b4a62c785
30209774 <saqib.mazhar@ucalgary.ca>
2025-03-26 23:05:12 -0600

    Created GUI Window for winning

---
+++ src/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // Load the win screen
+        Parent root = FXMLLoader.load(getClass().getResource("WinScreen.fxml"));
+        primaryStage.setTitle("You Win!");
+        primaryStage.setScene(new Scene(root, 700, 500));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
