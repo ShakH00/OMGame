a1aa5ff6b8eb65805c014a88cd2a95c10fee3e0a
30204732 <ethan.switzer@ucalgary.ca>
2025-03-28 18:19:07 -0600

    started admin clsss

--- src/authentication/Admin.java
+++ src/authentication/Admin.java
@@ -1,4 +1,27 @@
 package authentication;
 
 public class Admin {
+
+
+    /**
+     * Integer ID; unique for this Account, given by the Database. 16 chars long
+     */
+    private Integer id;
+
+    /**
+     * String for the username associated with this Account
+     */
+    private String username;
+
+    /**
+     * String for the email associated with this Account
+     */
+    private String email;
+
+    /**
+     * String for the password associated with this Account
+     */
+    private String password;
+
+
 }
