e46e7605c25a8e0fc030358cc73a495bcb584675
30204732 <ethan.switzer@ucalgary.ca>
2025-03-29 01:17:25 -0600

    added stubs for databaseManager

--- src/database/databaseManager.java
+++ src/database/databaseManager.java
@@ -23,6 +23,40 @@ public class databaseManager {
      *
      * queryPlayerPool returns a list of all players that are currently queued for a match
      */
-    public static Boolean getPlayer(Account account) {return true;}
+    public static ArrayList<Player> queryPlayerPool() {
+        return null;
+    }
+
+    /**
+     * @return Returns an account from the database given a phone number
+     * get account returns an account from the database
+     */
+    public static Account getAccount(Integer phoneNumber) {return new Account();}
+
+
+    /**
+     * @return Returns an account from the database
+     * get account returns an account from the database
+     */
+    public static Account getAccount(String email) {return new Account();}
+
+    /**
+     * @return returns true if an account was saved to the database
+     * saves an account to the database
+     */
+    public static Boolean saveAccount(Account account) {return true;}
+
+    /**
+     * @return returns true if an account was deleted from the database
+     *
+     */
+    public static Boolean deleteAccount(String email) {return true;}
+
+    /**
+     * @return returns true if an account was deleted from the database
+     *
+     */
+    public static Boolean deleteAccount(Integer userID) {return true;}
+
 
 }
