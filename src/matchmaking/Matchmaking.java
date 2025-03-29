4d464d3e85b21d634e52d02656c4eae327118650
30203751 <nebila.wako@ucalgary.ca>
2025-03-28 23:25:18 -0600

    Expansion after 2 minutes, 1 minute and 30 seconds

--- src/matchmaking/Matchmaking.java
+++ src/matchmaking/Matchmaking.java
@@ -66,6 +66,18 @@ public class Matchmaking {
 
         // Base threshold
         int baseThreshold = (rating1 < 1000 || rating2 < 1000) ? 150 : 100;
+
+        long now = System.currentTimeMillis();
+        long oldestJoinTime = Math.min(player1.getJoinTimestamp(), player2.getJoinTimestamp());
+        long waitTimeMillis = now - oldestJoinTime;
+
+        if (waitTimeMillis > 2 * 60 * 1000) { // after 2 minutes
+            baseThreshold = Integer.MAX_VALUE; // match with anyone
+        } else if (waitTimeMillis > 1 * 60 * 1000) { // after 1 minute
+            baseThreshold += 100;
+        } else if (waitTimeMillis > 30 * 1000) { // after 30 seconds
+            baseThreshold += 50;
+        }
     }
 
     /**
