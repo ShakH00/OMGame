7ca11b629091215fb6075217b494dd2e2650e9d6
alia shahid <alia.shahid@ucalgary.ca>
2025-03-30 02:46:52 -0600

    30215541 - created method for generate math problem for CAPTCHAAuthentication.java class.

--- src/authentication/CAPTCHAAuthentication.java
+++ src/authentication/CAPTCHAAuthentication.java
@@ -1,5 +1,12 @@
 package authentication;
 
-public class CAPTCHAAuthentication {
+public class CAPTCHAAuthentication (String mathProblem) throws CAPTCHAAuthenticationFailedException {
 
+    public static int generateMathProblem() {
+        Random rand = new Random();
+        int a = rand.nextInt(10) + 1;
+        int b = rand.nextInt(10) + 1;
+        int op = rand.nextInt(4);
+
+    }
 }
