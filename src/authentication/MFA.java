44b8b8e877151f0bf695b8d59bd369b3e33372c1
alia shahid <alia.shahid@ucalgary.ca>
2025-03-30 02:34:16 -0600

    Implemented user input, for verification mfa code.

--- src/authentication/MFA.java
+++ src/authentication/MFA.java
@@ -8,4 +8,16 @@ public static void MFA(String email) throws EmailAuthenticationFailedException {
     } else {
         code = "123456";
     }
+
+    System.out.printf("Verification code: %s", code, '\n');
+    System.out.println("Please enter verification code: ");
+    String userInput = s.nextLine();
+
+    if(userInput.equals(code)) {
+        System.out.println("Code verified");
+    }
+    else {
+        throw new EmailAuthenticationFailedException("Invalid code entered!");
+}
+
 }
