00fb114c95752fa46c307f066af6e9dd8218bacf
alia shahid <alia.shahid@ucalgary.ca>
2025-03-30 02:49:10 -0600

    30215541 - Completed generateMathProblem method.

--- src/authentication/CAPTCHAAuthentication.java
+++ src/authentication/CAPTCHAAuthentication.java
@@ -6,17 +6,27 @@ public class CAPTCHAAuthentication (String mathProblem) throws CAPTCHAAuthentica
         Random rand = new Random();
         int a = rand.nextInt(10) + 1;
         int b = rand.nextInt(10) + 1;
-        int op = rand.nextInt(4);
+        int operation = rand.nextInt(4);
 
         String problem = "";
         int answer = 0;
 
-        if (op == 0) {
+        if (operation == 0) {
             problem = a + " + " + b;
             answer = a + b;
-        } else if (op == 1) {
+        } else if (operation == 1) {
             problem = a + " - " + b;
             answer = a - b;
+        } else if (operation == 2) {
+            problem = a + " * " + b;
+            answer = a * b;
+        } else {
+            answer = a;
+            a = a * b; // ensures clean division
+            problem = a + " / " + b;
         }
+
+        System.out.println("Solve this CAPTCHA to continue: " + problem);
+        return answer;
     }
 }
