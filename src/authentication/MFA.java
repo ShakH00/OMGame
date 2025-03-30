c2f839104400c4b220aae0e786b00cf8f585ad7a
alia shahid <alia.shahid@ucalgary.ca>
2025-03-30 02:29:44 -0600

    Completed the first half of MFA. Added conditional statements for generating a random code.

--- src/authentication/MFA.java
+++ src/authentication/MFA.java
@@ -1,5 +1,11 @@
 package authentication;
 
-public class MFA {
-
+public static void MFA(String email) throws EmailAuthenticationFailedException {
+    Scanner sc = new Scanner(System.in);
+    String code;
+    if (testMode == false) {
+        code = generateRandomCode();
+    } else {
+        code = "123456";
+    }
 }
