1ddd1f561d9674f1949a8e39a69d683724031f42
alia shahid <alia.shahid@ucalgary.ca>
2025-03-30 23:29:09 -0600

    30215541 - Created test code for MFA in AuthenticationTestMain. java class.

--- src/authentication/AuthenticationTestMain.java
+++ src/authentication/AuthenticationTestMain.java
@@ -1,4 +1,22 @@
 package authentication;
 
+import ExceptionsAuthentication.MFAAuthenticationFailedException;
+
+import java.util.Scanner;
+
 public class AuthenticationTestMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("**** MFA Authentication Test ****");
+        System.out.print("Enter your email: ");
+        String email = sc.nextLine();
+
+        try {
+            MFAAuthentication.emailAuthenticatorDriver(email);
+        } catch (MFAAuthenticationFailedException e) {
+            System.out.println("MFA Authentication failed: " + e.getMessage());
+        }
+
+    }
 }
