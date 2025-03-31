248aec7826e55fab36cb612bcf55cbc587bb022b
30226959 <arwa.almousawi@ucalgary.ca>
2025-03-31 16:48:40 -0600

    minor changes to StartController to remove unused import statements

--- src/StartController.java
+++ src/StartController.java
@@ -1,20 +1,14 @@
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
-
-import javax.swing.*;
-import java.awt.event.MouseEvent;
 import java.io.IOException;
 
 public class StartController extends Application {
