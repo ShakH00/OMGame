303a2fb73eb79ce3122a8c61fa75a78a118c5b85
hassan <mhassan.haider24@gmail.com>
2025-04-01 01:35:18 -0600

    30217419 Added helper method for encryption class.

--- src/authentication/EncryptionAuthentication.java
+++ src/authentication/EncryptionAuthentication.java
@@ -1,4 +1,19 @@
 package authentication;
 
 public class EncryptionAuthentication {
+
+    private static String encryptString(String input) {
+        StringBuilder encrypted = new StringBuilder();
+        for (char character : input.toCharArray()) {
+            if (Character.isLetter(character)) {
+                char base = Character.isLowerCase(character) ? 'a' : 'A';
+                char shifted = (char) (((character - base + 3) % 26) + base);
+                encrypted.append(shifted);
+            } else {
+                throw new IllegalArgumentException("Non alphabetic characters are not supported.");
+            }
+        }
+        return encrypted.toString();
+    }
+
 }
