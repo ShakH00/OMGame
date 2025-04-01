b48f1529627d346bd35fc110d2fee9e5062fdabb
30204221 <abdulrahman.negmeldi@ucalgary.ca>
2025-03-31 19:31:41 -0600

    fixed piece attributes

--- src/MovingPiece.java
+++ src/MovingPiece.java
@@ -10,9 +10,28 @@
  * @author Abdulrahman
  */
 abstract class MovingPiece extends Piece{
-
+    private int x;
+    private int y;
     public MovingPiece(int x, int y, String colour, PieceType pieceType,  Player ownedBy, int score){
-        super(x, y, colour, pieceType, ownedBy, score);
+        super(colour, pieceType, ownedBy, score);
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
     }
 
     abstract void move(int currentX, int currentY, int newX, int newY, Board gameBoard);
