ecfdeb6687bf0981ca12cfc20d864e325e0c33af
hassan <mhassan.haider24@gmail.com>
2025-04-01 01:41:12 -0600

    30217419 Created a helper method for decryption class.

--- src/authentication/DecryptionAuthentication.java
+++ src/authentication/DecryptionAuthentication.java
@@ -1,4 +1,18 @@
 package authentication;
 
 public class DecryptionAuthentication {
+
+    private static String decryptString(String input) {
+        StringBuilder decrypted = new StringBuilder();
+        for (char character : input.toCharArray()) {
+            if (Character.isLetter(character)) {
+                char base = Character.isLowerCase(character) ? 'a' : 'A';
+                char shifted = (char) (((character - base - 3 + 26) % 26) + base);
+                decrypted.append(shifted);
+            } else {
+                throw new IllegalArgumentException("Non-alphabetic characters are not supported.");
+            }
+        }
+        return decrypted.toString();
+    }
 }
