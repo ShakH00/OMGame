1021e038a6ad978cd63254a29cf18719399c2f5d
30221927 <logan.olszak1@ucalgary.ca>
2025-04-02 17:25:10 -0600

    matchmaking now uses queuedFor instead of arraylists, now updates joinTimestamp when joining a queue

--- src/matchmaking/Matchmaking.java
+++ src/matchmaking/Matchmaking.java
@@ -10,22 +10,24 @@ public abstract class Matchmaking {
 
     /**
      * @author Logan Olszak
-     * @param account     Account object to add to the accountPool
-     * @param accountPool List of player objects that are currently queued
+     * @param account     Account object to add to the queue
+     * @param game        GameType that the account is queueing for
+     *
      * joinQueue adds a given player object into the list of player objects that are queued for a match
      */
-    public void joinQueue(Account account, ArrayList<Account> accountPool) {
-        accountPool.add(account);
+    public void joinQueue(Account account, GameType game) {
+        account.setQueuedFor(game);
+        account.setJoinTimestamp(System.currentTimeMillis());
     }
 
     /**
      * @author Logan Olszak
-     * @param account     Account object to remove from the accountPool
-     * @param accountPool List of Account objects that are currently queued
+     * @param account     Account object to remove from queue
+     *
      * removeFromQueue removes a given Account from the list of Accounts that are queued for a match
      */
-    public void removeFromQueue(Account account, ArrayList<Account> accountPool) {
-        accountPool.remove(account);
+    public void removeFromQueue(Account account) {
+        account.clearQueuedFor();
     }
 
     /**
