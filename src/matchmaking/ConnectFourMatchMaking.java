672b3f16ce4670e6c433ab88a4f6d80e99c56794
30203751 <nebila.wako@ucalgary.ca>
2025-04-01 20:08:20 -0600

    Increase range template for all games modify expansions using matchmaking.md

--- src/matchmaking/ConnectFourMatchMaking.java
+++ src/matchmaking/ConnectFourMatchMaking.java
@@ -7,9 +7,28 @@ public class ConnectFourMatchMaking extends Matchmaking {
     private int increment;
 
 
-    public void increaseRange(int elo) {
+    public boolean increaseRange(Player player1, Player player2) {
+        int rating1 = player1.getElo();
+        int rating2 = player2.getElo();
 
+        // Base threshold
+        int baseThreshold = (rating1 < 1000 || rating2 < 1000) ? 150 : 100;
+
+        long now = System.currentTimeMillis();
+        long oldestJoinTime = Math.min(player1.getJoinTimestamp(), player2.getJoinTimestamp());
+        long waitTimeMillis = now - oldestJoinTime;
+
+        if (waitTimeMillis > 2 * 60 * 1000) { // after 2 minutes
+            baseThreshold = Integer.MAX_VALUE; // match with anyone
+        } else if (waitTimeMillis > 1 * 60 * 1000) { // after 1 minute
+            baseThreshold += 100;
+        } else if (waitTimeMillis > 30 * 1000) { // after 30 seconds
+            baseThreshold += 50;
+        }
+        //TEMP VALUE
+        return false;
     }
+
 
     public void findTwoPlayers() {
 
--- src/matchmaking/Matchmaking.java
+++ src/matchmaking/Matchmaking.java
@@ -7,39 +7,8 @@ import java.util.List;
 
 public class Matchmaking {
 
-    public boolean isMatchSuitable(Player player1, Player player2) {
-        int rating1 = player1.getElo();
-        int rating2 = player2.getElo();
 
-        // Base threshold
-        int baseThreshold = (rating1 < 1000 || rating2 < 1000) ? 150 : 100;
 
-        long now = System.currentTimeMillis();
-        long oldestJoinTime = Math.min(player1.getJoinTimestamp(), player2.getJoinTimestamp());
-        long waitTimeMillis = now - oldestJoinTime;
-
-        if (waitTimeMillis > 2 * 60 * 1000) { // after 2 minutes
-            baseThreshold = Integer.MAX_VALUE; // match with anyone
-        } else if (waitTimeMillis > 1 * 60 * 1000) { // after 1 minute
-            baseThreshold += 100;
-        } else if (waitTimeMillis > 30 * 1000) { // after 30 seconds
-            baseThreshold += 50;
-        }
-        //TEMP VALUE
-        return false;
-    }
-
-    /**
-     * Finds the best match for a player from a pool
-     *
-     * @param targetPlayer
-     * @param playerPool
-     * @return
-     */
-
-    public Player findBestMatch(Player targetPlayer, List<Player> playerPool) {
-        return null;
-    }
 
     /**
      * @author Logan Olszak
