ff0949acf62300db24619e6b1ddd94dfb000e7fe
30207936 <shakil.hussain1@ucalgary.ca>
2025-04-02 22:23:06 -0600

    fixes issues with fonts.

--- src/UserProfileController.java
+++ src/UserProfileController.java
@@ -27,7 +27,12 @@ public class UserProfileController extends Application {
             Scene scene = new Scene(loader.load(), 800, 570);
 
             String fontPath = getClass().getResource("resources/fonts/PressStart2P-Regular.ttf").toExternalForm();
+            String retroGamingPath = getClass().getResource("resources/fonts/RetroGaming.ttf").toExternalForm();
+            String pixelitePath = getClass().getResource("resources/fonts/Pixelite.ttf").toExternalForm();
+
             Font pressStartFont = Font.loadFont(fontPath, 40);
+            Font retroGamingFont = Font.loadFont(retroGamingPath, 40);
+            Font pixeliteFont = Font.loadFont(pixelitePath, 40);
             scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
 
 
