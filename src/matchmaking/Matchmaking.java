58e723a791d746729cdfbcdeb096debcd5499bc4
30211705 <elijah.mickelson@ucalgary.ca>
2025-04-03 03:13:41 -0600

    Fixed errors in Matchmaking, but still need to implement networking functions to store and regularly iterate through list of players in matchmaking

--- src/matchmaking/Matchmaking.java
+++ src/matchmaking/Matchmaking.java
@@ -3,11 +3,7 @@ package matchmaking;
 import account.Account;
 import game.GameType;
 
-import java.util.ArrayList;
-
 public abstract class Matchmaking {
-    final private GameType gameType = null;
-
     /**
      * @author Logan Olszak
      * @param account     Account object to add to the queue
@@ -18,6 +14,7 @@ public abstract class Matchmaking {
     public void joinQueue(Account account, GameType game) {
         account.setQueuedFor(game);
         account.setJoinTimestamp(System.currentTimeMillis());
+        updateMatchmakingRange(account);    // TODO: handle this through a server so that matchmaking can actually occur
     }
 
     /**
@@ -31,30 +28,50 @@ public abstract class Matchmaking {
     }
 
     /**
-     * @author Nebila Wako
-     * @param account1      ?
-     * @param account2      ?
-     * @return              ?
+     * Update the matchmaking range for the Account based on the game and time waited.
+     * @author Nebila Wako, Elijah Mickelson
+     * @param account      Account to recalculate Matchmaking range for
+     * @return             true if the Account matchmaking range was updated; false otherwise
      */
-    public boolean increaseRange(Account account1, Account account2) {
-        int rating1 = account1.getElo(gameType);
-        int rating2 = account2.getElo(gameType);
+    public boolean updateMatchmakingRange(Account account) {
+        // If the user is not matchmaking, return false.
+        if (account.getQueuedFor() == null) { return false; }
 
-        // Base threshold
-        int baseThreshold = (rating1 < 1000 || rating2 < 1000) ? 150 : 100;
+        // Get the game that the user is matchmaking for and their elo in that game
+        GameType game = account.getQueuedFor();
+        int elo = account.getElo(game);
 
-        long now = System.currentTimeMillis();
-        long oldestJoinTime = Math.min(account1.getJoinTimestamp(), account2.getJoinTimestamp());
-        long waitTimeMillis = now - oldestJoinTime;
+        // Base matchmaking threshold based on account elo
+        int baseThreshold = (elo < 1000) ? 150 : 100;
 
-        if (waitTimeMillis > 2 * 60 * 1000) { // after 2 minutes
-            baseThreshold = Integer.MAX_VALUE; // match with anyone
-        } else if (waitTimeMillis > 60 * 1000) { // after 1 minute
-            baseThreshold += 100;
-        } else if (waitTimeMillis > 30 * 1000) { // after 30 seconds
-            baseThreshold += 50;
+        // Get time (in ms) that the account has been waiting
+        int timeWaitSec = (int) (System.currentTimeMillis() - account.getJoinTimestamp()) / 1000;
+
+        // Get the rate of threshold increase per 30 seconds (dependent on game)
+        int thresholdIncreaseRate;
+        switch (game) {
+            case CHESS              ->  thresholdIncreaseRate = 50;
+            case CHECKERS, CONNECT4 ->  thresholdIncreaseRate = 75;
+            case TICTACTOE          ->  thresholdIncreaseRate = 100;
+            default                 ->  thresholdIncreaseRate = 0;
         }
-        //TEMP VALUE
+
+        // Calculate final threshold
+        int new_threshold;
+        if (timeWaitSec < 120) {
+            new_threshold = baseThreshold + (timeWaitSec / 30) * thresholdIncreaseRate;
+        }
+        // After 2 minutes, match anyone regardless of skill
+        else {
+            new_threshold = Integer.MAX_VALUE;
+        }
+
+        // If threshold changed, return true and update matchmaking threshold for the Account
+        if (new_threshold != account.getMatchmakingThreshold()) {
+            account.setMatchmakingThreshold(new_threshold);
+            return true;
+        }
+        // Otherwise, return false
         return false;
     }
 }
