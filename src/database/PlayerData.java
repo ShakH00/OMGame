8db5267cd56a4cf9613a449c2fb365c3a3949ec8
hassan <mhassan.haider24@gmail.com>
2025-04-03 01:10:45 -0600

    30217419 Created method to add a player ibn PlayerData.java

--- src/database/PlayerData.java
+++ src/database/PlayerData.java
@@ -8,4 +8,10 @@ public class PlayerData {
 
     public static ArrayList<Player> players = new ArrayList<>();
 
+    //Method to add a player to the database
+    public static void addPlayer(Player player) {
+        players.add(player);
+    }
+
+
 }
