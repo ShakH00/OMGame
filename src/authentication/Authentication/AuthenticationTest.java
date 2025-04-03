99897468720a9105cbc795bbd990885e19f446cc
alia shahid <alia.shahid@ucalgary.ca>
2025-04-03 01:33:58 -0600

    30215541 - Added test case for MFA if user enters wrong code.

--- src/authentication/Authentication/AuthenticationTest.java
+++ src/authentication/Authentication/AuthenticationTest.java
@@ -1,4 +1,5 @@
 package authentication.Authentication;
+import ExceptionsAuthentication.MFAAuthenticationFailedException;
 import authentication.CAPTCHAAuthentication;
 import authentication.MFAAuthentication;
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,24 @@ public class AuthenticationTest {
 
     @Test
     public void MFATest1() {
-        MFAAuthentication.testMode = true; // Enable test mode to use code "246810"
-        String simulatedUserInput = "246810\n";
-        System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
+        MFAAuthentication.testMode = true;
+        String userInput = "246810\n";
+        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
 
-        assertDoesNotThrow(() -> MFAAuthentication.emailAuthenticatorDriver("user@example.com"));
+        assertDoesNotThrow(() -> MFAAuthentication.emailAuthenticatorDriver("user@ucalgary.ca"));
     }
+
+    @Test
+    public void MFATest2() {
+        MFAAuthentication.testMode = true; // force known code "123456"
+        String userInput = "000000\n"; // user inputting wrong input
+        System.setIn(new ByteArrayInputStream(userInput.getBytes()));
+
+        assertThrows(MFAAuthenticationFailedException.class, () -> MFAAuthentication.emailAuthenticatorDriver("user@ucalgary.ca")
+        );
+    }
+
+
 
 
 }
