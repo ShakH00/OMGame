da81eb8c64d6ee6f20cd0c147cd156cec2d49815
alia shahid <alia.shahid@ucalgary.ca>
2025-04-03 23:02:27 -0600

    30215541 - commented out code in AuthenticationTestMain.java class for code testing.

--- src/authentication/AuthenticationTestMain.java
+++ src/authentication/AuthenticationTestMain.java
@@ -6,26 +6,26 @@ import ExceptionsAuthentication.MFAAuthenticationFailedException;
 import java.util.Scanner;
 
 public class AuthenticationTestMain {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("**** MFA Authentication Test ****");
-        System.out.print("Enter your email: ");
-        String email = sc.nextLine();
-
-        try {
-            MFAAuthentication.emailAuthenticatorDriver(email);
-        } catch (MFAAuthenticationFailedException e) {
-            System.out.println("MFA Authentication failed: " + e.getMessage());
-        }
-        System.out.println("\n**** CAPTCHA Authentication Test ****");
-        System.out.print("Enter your answer to the CAPTCHA equation: ");
-        String captchaInput = sc.nextLine();
-
-        try {
-            CAPTCHAAuthentication.captchaAuthenticatorDriver(captchaInput);
-        } catch (CAPTCHAAuthenticationFailedException e) {
-            System.out.println("CAPTCHA Authentication failed: " + e.getMessage());
-        }
-    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("**** MFA Authentication Test ****");
+//        System.out.print("Enter your email: ");
+//        String email = sc.nextLine();
+//
+//        try {
+//            MFAAuthentication.emailAuthenticatorDriver(email);
+//        } catch (MFAAuthenticationFailedException e) {
+//            System.out.println("MFA Authentication failed: " + e.getMessage());
+//        }
+//        System.out.println("\n**** CAPTCHA Authentication Test ****");
+//        System.out.print("Enter your answer to the CAPTCHA equation: ");
+//        String captchaInput = sc.nextLine();
+//
+//        try {
+//            CAPTCHAAuthentication.captchaAuthenticatorDriver(captchaInput);
+//        } catch (CAPTCHAAuthenticationFailedException e) {
+//            System.out.println("CAPTCHA Authentication failed: " + e.getMessage());
+//        }
+//    }
 }
