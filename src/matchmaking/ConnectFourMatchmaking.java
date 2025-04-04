e182cd896d08e7033aa0b089f538aa0fe20eb022
30207936 <shakil.hussain1@ucalgary.ca>
2025-04-03 23:18:51 -0600

    revert patch

--- src/matchmaking/ConnectFourMatchmaking.java
+++ src/matchmaking/ConnectFourMatchmaking.java
@@ -1,6 +1,6 @@
 package matchmaking;
 
-public class ConnectFourMatchMaking extends MatchMaking {
+public class ConnectFourMatchmaking extends MatchMaking {
     private long startingTime;
     private int findElo;
     private int increment;
