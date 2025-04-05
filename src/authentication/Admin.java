96a11800b0a368ef6c79b3f98c9794545982de98
30211705 <elijah.mickelson@ucalgary.ca>
2025-04-04 21:16:42 -0600

    Fixed error in Admin.java caused by changing friend references to use ID

--- src/authentication/Admin.java
+++ src/authentication/Admin.java
@@ -31,7 +31,7 @@ public class Admin {
      * @param newPassword the new password to set
      */
     private void updatePassword(DatabaseManager db, int id, String newPassword){
-        Account account = db.queryAccountByEmail(id);
+        Account account = db.queryAccountByID(id);
         account.setPassword(newPassword);
         db.saveAccount(account);
     }
@@ -44,7 +44,7 @@ public class Admin {
      * @param newUsername the new username to set
      */
     private void updateUsername(DatabaseManager db, int id, String newUsername){
-        Account account = db.queryAccountByEmail(id);
+        Account account = db.queryAccountByID(id);
         account.setPassword(newUsername);
         db.saveAccount(account);
     }
