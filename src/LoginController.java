00365f5d64653d97679642e68cf654e3cf494478
30209416 <emily.morozoff1@ucalgary.ca>
2025-04-04 18:15:29 -0700

    Added to and Updated Login page

--- src/LoginController.java
+++ src/LoginController.java
@@ -3,17 +3,26 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController extends Application {
+
     @FXML
     AnchorPane rootPane;
     @FXML
-    private Label closeButton;
+    private StackPane backButtonLogin;
+    @FXML
+    private StackPane toSignUp;
+    @FXML
+    private StackPane submitButton;
+    @FXML
+    private StackPane guestButton;
 
     @Override
     public void start(Stage primaryStage) {
@@ -32,19 +41,17 @@ public class LoginController extends Application {
             e.printStackTrace();
         }
     }
+
     @FXML
-    public void signup(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
-        // load help.fxml
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("screens/Signup.fxml"));
-        Parent helpRoot = loader.load();
+    private void switchToSignup(javafx.scene.input.MouseEvent mouseEvent) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        SceneManager.switchScene(stage, "screens/Signup.fxml");
+    }
 
-        // get help controller
-        SignUpController signUpController = loader.getController();
-
-        rootPane.getChildren().add(helpRoot);  // rootPane is the main container in Start.fxml
-
-        // set the helpRoot visible (it will be hidden initially)
-        helpRoot.setVisible(true);
+    @FXML
+    private void switchToHome(javafx.scene.input.MouseEvent mouseEvent) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        SceneManager.switchScene(stage, "screens/Start.fxml");
     }
 
     public static void main(String[] args) {
