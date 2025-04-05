e17aa786c24920787ce0234600e317c732514774
30209416 <emily.morozoff1@ucalgary.ca>
2025-04-04 19:06:55 -0700

    Added to and Updated Login animations

--- src/LoginController.java
+++ src/LoginController.java
@@ -1,3 +1,6 @@
+import javafx.animation.FillTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -5,9 +8,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -23,6 +30,8 @@ public class LoginController extends Application {
     private StackPane submitButton;
     @FXML
     private StackPane guestButton;
+    @FXML
+    private Text guestText;
 
     @Override
     public void start(Stage primaryStage) {
@@ -42,6 +51,13 @@ public class LoginController extends Application {
         }
     }
 
+    public void initialize() {
+        StartController.createScaleTransition(backButtonLogin);
+        StartController.createScaleTransition(toSignUp);
+        StartController.createScaleTransition(submitButton);
+        StartController.createScaleTransition(guestButton);
+    }
+
     @FXML
     private void switchToSignup(javafx.scene.input.MouseEvent mouseEvent) {
         Stage stage = (Stage) rootPane.getScene().getWindow();
@@ -53,6 +69,13 @@ public class LoginController extends Application {
         Stage stage = (Stage) rootPane.getScene().getWindow();
         SceneManager.switchScene(stage, "screens/Start.fxml");
     }
+
+    @FXML
+    private void switchToGameSelect(javafx.scene.input.MouseEvent mouseEvent) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        SceneManager.switchScene(stage, "screens/gameSelect.fxml");
+    }
+
 
     public static void main(String[] args) {
         launch(args);
