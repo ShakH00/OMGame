d0b6503823b221b0e8ad4a9f6093707021c5a585
30207936 <shakil.hussain1@ucalgary.ca>
2025-04-07 17:04:20 -0600

    adds cartridge functionality for all games (chess broken cus its still not finished)

--- src/GameSelectController.java
+++ src/GameSelectController.java
@@ -1,17 +1,14 @@
-import javafx.animation.ScaleTransition;
-import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
-import javafx.util.Duration;
 
 
 public class GameSelectController extends Application {
@@ -39,6 +36,16 @@ public class GameSelectController extends Application {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("screens/GameSelect.fxml"));
             Scene scene = new Scene(loader.load(), 800, 570);
 
+            String fontPath = getClass().getResource("resources/fonts/PressStart2P-Regular.ttf").toExternalForm();
+            String retroGamingPath = getClass().getResource("resources/fonts/RetroGaming.ttf").toExternalForm();
+            String pixelitePath = getClass().getResource("resources/fonts/Pixelite.ttf").toExternalForm();
+
+            Font pressStartFont = Font.loadFont(fontPath, 40);
+            Font retroGamingFont = Font.loadFont(retroGamingPath, 40);
+            Font pixeliteFont = Font.loadFont(pixelitePath, 40);
+
+            scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
             ImageView gifView = new ImageView(new Image(getClass().getResource("/images/twinklingstars.gif").toExternalForm()));
 
             StackPane root = new StackPane();
@@ -60,10 +67,10 @@ public class GameSelectController extends Application {
 
     public void initialize() {
         UtilityManager.createScaleTransition(backButton);
-        UtilityManager.createtranslationTransition(greenCartridge);
-        UtilityManager.createtranslationTransition(pinkCartridge);
-        UtilityManager.createtranslationTransition(purpleCartridge);
-        UtilityManager.createtranslationTransition(orangeCartridge);
+        UtilityManager.createTranslationTransition(greenCartridge);
+        UtilityManager.createTranslationTransition(pinkCartridge);
+        UtilityManager.createTranslationTransition(purpleCartridge);
+        UtilityManager.createTranslationTransition(orangeCartridge);
     }
 
     @FXML
@@ -71,35 +78,36 @@ public class GameSelectController extends Application {
         Stage stage = (Stage) rootPane.getScene().getWindow();
         SceneManager.switchScene(stage, "screens/Start.fxml");
     }
+
     @FXML
     private void switchToProfile(javafx.scene.input.MouseEvent mouseEvent) {
         Stage stage = (Stage) rootPane.getScene().getWindow();
         SceneManager.switchScene(stage, "screens/UserProfile.fxml");
     }
+
     @FXML
     private void switchToConnect4(javafx.scene.input.MouseEvent mouseEvent) {
         Stage stage = (Stage) rootPane.getScene().getWindow();
         SceneManager.switchScene(stage, "screens/Connect4.fxml");
     }
-    // TODO; connect when screens done
-//    @FXML
-//    private void switchToTicTacToe(javafx.scene.input.MouseEvent mouseEvent) {
-//        Stage stage = (Stage) rootPane.getScene().getWindow();
-//        SceneManager.switchScene(stage, "screens/TicTacToe.fxml");
-//    }
 
+    @FXML
+    private void switchToTicTacToe(javafx.scene.input.MouseEvent mouseEvent) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        SceneManager.switchScene(stage, "screens/TicTacToe.fxml");
+    }
 
-//    @FXML
-//    private void switchToChess(javafx.scene.input.MouseEvent mouseEvent) {
-//        Stage stage = (Stage) rootPane.getScene().getWindow();
-//        SceneManager.switchScene(stage, "screens/Chess.fxml");
-//    }
+    @FXML
+    private void switchToChess(javafx.scene.input.MouseEvent mouseEvent) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        SceneManager.switchScene(stage, "screens/Chess.fxml");
+    }
 
-//    @FXML
-//    private void switchToCheckers(javafx.scene.input.MouseEvent mouseEvent) {
-//        Stage stage = (Stage) rootPane.getScene().getWindow();
-//        SceneManager.switchScene(stage, "screens/Checkers.fxml");
-//    }
+    @FXML
+    private void switchToCheckers(javafx.scene.input.MouseEvent mouseEvent) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        SceneManager.switchScene(stage, "screens/Checkers.fxml");
+    }
 
 
     public static void main(String[] args) {
