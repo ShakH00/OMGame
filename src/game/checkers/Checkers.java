1030bdffe9184b676aec7666343f0937a57fa2bd
30228810 <charls.coronel@ucalgary.ca>
2025-04-07 12:02:51 -0600

    Added and fixed javadocs for most methods. Created variables to keep track of statistics for leaderboard. Modified move method to take into considerations these counters. Added getters for each counters for leaderboard to collect. Removed duplicate draw method. Created a method to display the result of the game when match concludes. Removed unused variables.

--- src/game/checkers/Checkers.java
+++ src/game/checkers/Checkers.java
@@ -15,9 +15,6 @@ import java.util.concurrent.LinkedTransferQueue;
 public class Checkers extends Game {
     private Player player1;
     private Player player2;
-    private int score;
-    private Piece[] capturedP1;
-    private Piece[] capturedP2;
     private Piece selectedPiece;
     private Board board;
     private GameState gameState;
@@ -27,12 +24,26 @@ public class Checkers extends Game {
         this.player2 = new Player();
     }
 
+    // Leaderboard Statistics
+    private int p1Turns = 0;
+    private int p2Turns = 0;
+    private int p1Captures = 0;
+    private int p2Captures = 0;
+    private int p1MultiCaptures = 0;
+    private int p2MultiCaptures = 0;
+
     // Set Board for checkers game and fill it
     public void setBoard(Board board){
         this.board = board;
         board.fillBoard(GameType.CHECKERS);
     }
 
+    /**
+     * Method to move a piece to a new x and y coordinate
+     * @param piece: piece being moved.
+     * @param newX: new x coordinate
+     * @param newY: new y coordinate
+     */
     public void move(CheckersPiece piece, int newX, int newY){
         int currentX = piece.getX();
         int currentY = piece.getY();
@@ -55,6 +66,16 @@ public class Checkers extends Game {
             piece.setX(newX);
             piece.setY(newY);
 
+            // Take count of turns made by players.
+            // If player1 made the turn increase p1Turns by 1.
+            if (piece.getColor().equals(Color.BLACK)){
+                p1Turns++;
+            }
+            else {
+                // Else player2 made the turn so increase p2Turns by 1.
+                p2Turns++;
+            }
+
             // Capture move.
             // If the move made is 2 cells it is a capture move.
             if (captureMove){
@@ -63,6 +84,16 @@ public class Checkers extends Game {
                 int capturedY = (currentY + newY) / 2;
                 // remove the captured piece.
                 boardState[capturedX][capturedY] = null;
+
+                // Take count of captures made by players.
+                // If player1 made the capture increase p1Captures by 1.
+                if (piece.getColor().equals(Color.BLACK)){
+                    p1Captures++;
+                }
+                else {
+                // Else player2 made the capture so increase p2Captures by 1.
+                    p2Captures++;
+                }
             }
 
             // game.chess.King promotion, assuming black is p1 and red is p2
@@ -73,6 +104,16 @@ public class Checkers extends Game {
 
             // Multiple captures available
             if (captureMove && canCapture(piece)){
+                // Take count of multi captures made by players.
+                // If player1 is the one making the multi capture then increase p1MultiCaptures by 1.
+                if (piece.getColor().equals(Color.BLACK)){
+                    p1MultiCaptures++;
+                }
+                else {
+                // Else player2 is the one making the multi capture so increase p2MultiCaptures by 1.
+                    p2MultiCaptures++;
+                }
+
                 System.out.println("Continuous jumps are available, continue capturing.");
             }
             else {
@@ -83,8 +124,10 @@ public class Checkers extends Game {
     }
 
     /**
-     * Check the board and the player's piece to see if any capture is available.
-     * captures are forced in checkers.
+     * Method to check the board and the player's piece to see if any capture is available.
+     * Captures are forced in checkers.
+     * @param currentPlayer: To check the piece owner
+     * @return true if player has to capture.
      */
     public boolean forcedCapture (Player currentPlayer) {
         // Get the board
@@ -106,7 +149,9 @@ public class Checkers extends Game {
     }
 
     /**
-     * check if a piece can capture.
+     * Helper method for forcedCapture method, checks if the piece owned by the player has any possible captures.
+     * @param piece: The piece being checked
+     * @return true if piece is able to capture.
      */
     private boolean canCapture(CheckersPiece piece) {
         int x = piece.getX();
@@ -131,9 +176,13 @@ public class Checkers extends Game {
     }
 
     /**
-     * [!] Should we consider a draw occurring when players repeat move? (TBD later)
-     * [!] NOT COMPLETE NEED TO CHECK CAPTURE VALUES ~ Adam
-     * 
+     * Method validate movement of a piece to a new location.
+     * @param currentX: current x coordinate of the piece
+     * @param currentY: current y coordinate of the piece
+     * @param newX: new x coordinate being moved to
+     * @param newY: new y coordinate being moved to
+     * @param gameBoard: board being played on
+     * @return true if the selected piece can move to its new location.
      */
     public boolean isValidMove(int currentX, int currentY, int newX, int newY, Board gameBoard){
         Piece[][] board = gameBoard.getBoardState();
@@ -239,13 +288,7 @@ public class Checkers extends Game {
 
 
     /**
-     * Check game.pieces on the board.
-     * Piece 1 = game.Player 1, BLACK
-     * Piece 2 = game.Player 2, RED
-     * If !Piece1Exists then P2 Wins
-     * If !Piece2Exists then P1 Wins.
-     * [!!!] How do we decide colours? Let colour be red/black and GUI do the rest?
-     *
+     * Method to checkWinCondition and update the GameState depending on which player has missing pieces.
      */
     public void checkWinCondition(){
         boolean Piece1Exists = false;
@@ -275,7 +318,7 @@ public class Checkers extends Game {
 
     /**
      * surrender method that changes the gameState to the opposing player winning if a player calls it during their turn.
-     * called with a button in controller.
+     * Called with a button in controller.
      */
     public void surrender(){
         if (gameState == GameState.P1_TURN){
@@ -285,34 +328,95 @@ public class Checkers extends Game {
         }
     }
 
+    /**
+     * draw method that changes Game State to draw.
+     */
     public void draw(){
         gameState = GameState.DRAW;
     }
 
     /**
-     * Match Outcome, checks the state of the game and depending on it,
-     * will set out a line stating which player won or if the game is still ongoing otherwise.
-     *
+     * matchOutcome method checks the state of the game and sends out a string to inform the status of the game.
      */
     public void matchOutcome(){
         if(gameState == GameState.P1_WIN){
             System.out.println("Player 1 has won the game!");
+            displayMatchStatistics();
         }
         else if(gameState == GameState.P2_WIN) {
             System.out.println("Player 2 has won the game!");
-        } else if (gameState == GameState.DRAW) {
+            displayMatchStatistics();
+        }
+        else if (gameState == GameState.DRAW) {
             System.out.println("Both players have agreed to a draw!");
-        } else{
+            displayMatchStatistics();
+        }
+        else{
             System.out.println("The game is ongoing");
         }
     }
 
-    public void drawGame()
-    {
-        gameState = GameState.DRAW;
-    }
-
+    /**
+     * getGameState getter method.
+     * @return current gameState
+     */
     public GameState getGameState() {
         return gameState;
+    }
+
+    /**
+     * getter method to get turns made by P1
+     * @return Turns made by player 1 in int.
+     */
+    public int getP1Turns() {
+        return p1Turns;
+    }
+
+    /**
+     * getter method to get turns made by P2
+     * @return Turns made by player 2 in int.
+     */
+    public int getP2Turns() {
+        return p2Turns;
+    }
+
+    /**
+     * getter method to get captures made by P1
+     * @return captures made by player 1 in int.
+     */
+    public int getP1Captures() {
+        return p1Captures;
+    }
+
+
+    /**
+     * getter method to get captures made by P2
+     * @return captures made by player 2 in int.
+     */
+    public int getP2Captures() {
+        return p2Captures;
+    }
+
+
+    /**
+     * getter method to get multi captures made by P1
+     * @return multi captures made by player 1 in int.
+     */
+    public int getP1MultiCaptures() {
+        return p1MultiCaptures;
+    }
+
+    /**
+     * getter method to get multi captures made by P2
+     * @return multi captures made by player 2 in int.
+     */
+    public int getP2MultiCaptures() {
+        return p2MultiCaptures;
+    }
+
+    public void displayMatchStatistics(){
+        System.out.println("Game Statistics: ");
+        System.out.println("Player 1 Turns: " + getP1Turns() + ", Captures: " + getP1Captures() + ", Multi-Captures: " + getP1MultiCaptures());
+        System.out.println("Player 2 Turns: " + getP2Turns() + ", Captures: " + getP2Captures() + ", Multi-Captures: " + getP2MultiCaptures());
     }
 }
