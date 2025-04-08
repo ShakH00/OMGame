b24c0c2f67ae69295f0ff38c2bb61a91676c42bf
30204732 <ethan.switzer@ucalgary.ca>
2025-04-08 13:44:18 -0600

    created adminController

---
+++ src/AdminController.java
@@ -0,0 +1,80 @@
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+public class AdminController extends Application {
+    @FXML
+    AnchorPane rootPane;
+    @FXML
+    private StackPane findUser;
+    @FXML
+    private StackPane clearStats;
+    @FXML
+    private StackPane deleteUser;
+    @FXML
+    private StackPane submitButton;
+    @FXML
+    private TextField IDField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("screens/Signup.fxml"));
+            Scene scene = new Scene(loader.load(), 800, 570);
+
+            String fontPath = getClass().getResource("resources/fonts/PressStart2P-Regular.ttf").toExternalForm();
+            String retroGamingPath = getClass().getResource("resources/fonts/RetroGaming.ttf").toExternalForm();
+            String pixelitePath = getClass().getResource("resources/fonts/Pixelite.ttf").toExternalForm();
+
+            Font pressStartFont = Font.loadFont(fontPath, 40);
+            Font retroGamingFont = Font.loadFont(retroGamingPath, 40);
+            Font pixeliteFont = Font.loadFont(pixelitePath, 40);
+
+            scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+            primaryStage.setResizable(false);
+
+            // Set up the primary stage
+            primaryStage.setTitle("OMG!");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void initialize() {
+        UtilityManager.createScaleTransition(backButtonSignUp);
+        UtilityManager.createScaleTransition(toLogin);
+        UtilityManager.createScaleTransition(submitButton);
+        UtilityManager.colourTransition(guestText);
+    }
+
+    @FXML
+    private void switchToLogin(javafx.scene.input.MouseEvent mouseEvent) {
+        Stage stage = (Stage) rootPane.getScene().getWindow();
+        SceneManager.switchScene(stage, "screens/Login.fxml");
+    }
+
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
