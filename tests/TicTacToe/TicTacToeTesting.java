bd7f122b0cdf5ada29cf4ff13269c4081b42128e
30213715 <zhuoxi.hong1@ucalgary.ca>
2025-04-08 01:29:37 -0600

    add game stats (not done though)

--- tests/TicTacToe/TicTacToeTesting.java
+++ tests/TicTacToe/TicTacToeTesting.java
@@ -9,7 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class TicTacToeTestingTest {
+public class TicTacToeTesting {
 
     private TicTacToe tictactoe;
 
