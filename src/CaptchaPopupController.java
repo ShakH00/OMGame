5d3754aa9909f6a773919ddb3f34609cb2427f79
30207936 <shakil.hussain1@ucalgary.ca>
2025-04-10 17:47:57 -0600

    redirects to match type

--- src/CaptchaPopupController.java
+++ src/CaptchaPopupController.java
@@ -63,6 +63,8 @@ public class CaptchaPopupController extends Application  {
             textBased.setText(cap.getInstructions());
             prompt.setText(cap.getPrompt());
         }
+
+        UtilityManager.createScaleTransition(submitButton);
     }
 
 
