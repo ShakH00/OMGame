9081a887771122daf7514b5a149982235595bc9f
Amr Ibrahim <amr.ibrahim1@ucalgary.ca>
2025-04-10 19:30:56 -0600

    Load the fonts added

--- src/UserSettingsController.java
+++ src/UserSettingsController.java
@@ -1,21 +1,56 @@
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class UserSettingsController extends Application {
 
+    @FXML
+    private Label nameDisplay;
+
+    @FXML
+    private TextField displayNameField;
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/screens/UserSettings.fxml"));
-        Scene scene = new Scene(root, 950, 600);
-        primaryStage.setTitle("User Settings");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("screens/UserSettings.fxml"));
+            Scene scene = new Scene(loader.load(), 950, 600);
+
+            // Load custom fonts
+            String pressStartFont = getClass().getResource("resources/fonts/PressStart2P-Regular.ttf").toExternalForm();
+            String retroGamingFont = getClass().getResource("resources/fonts/RetroGaming.ttf").toExternalForm();
+            String pixeliteFont = getClass().getResource("resources/fonts/Pixelite.ttf").toExternalForm();
+
+            Font.loadFont(pressStartFont, 40);
+            Font.loadFont(retroGamingFont, 40);
+            Font.loadFont(pixeliteFont, 40);
+
+            scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+            primaryStage.setTitle("User Settings");
+            primaryStage.setResizable(false);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void initialize() {
+        if (nameDisplay != null && displayNameField != null) {
+            nameDisplay.textProperty().bind(displayNameField.textProperty());
+        }
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
