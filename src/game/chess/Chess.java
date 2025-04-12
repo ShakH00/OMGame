f248e600023000707a5b406c374f5371cb1fac5d
30204221 <abdulrahman.negmeldi@ucalgary.ca>
2025-04-11 23:02:56 -0600

    prevent castling while checked

--- src/game/chess/Chess.java
+++ src/game/chess/Chess.java
@@ -138,9 +138,9 @@ public class Chess extends Game {
         // Actually perform the move
 
         Piece potentialEnemy = board.getBoardState()[x][y];
-        if(piece instanceof King){
-            if(isKingInCheck(player) && ((King) piece).isTryingToCastle(oldX, oldY, x, y, this.board)) return;
-        }
+
+        if(piece instanceof King && isKingInCheck(player) && ((King) piece).isTryingToCastle(oldX, oldY, x, y, this.board)) return;
+
         if (piece.move(x, y, board)) {
             if(piece.getOwnedBy() == this.player1){
                 addp1Turn();
